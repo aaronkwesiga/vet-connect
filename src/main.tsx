dcf5317f2f2919d8f0c0c5589f10b041a7500101
Aaron kwesiga <aaronkwesiga20@gmail.com>
2025-11-25 19:12:08 +0300

    initial

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
+// main.tsx
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { HashRouter } from "react-router-dom"; // <-- use HashRouter
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
